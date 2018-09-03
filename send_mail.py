1d57f90c83768e7b53b43cc9236c9fa675dbc591
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-09-03 18:56:23 +0530

    .env.example added

--- send_mail.py
+++ send_mail.py
@@ -2,21 +2,22 @@
 
 from flask import Flask
 from flask_mail import Mail, Message
+from decouple import config
 
 app =Flask(__name__)
 mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = config('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = config('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = '', recipients = ['rajulbhargav23@gmail.com','001adarshshrivastava@gmail.com','hashukumar01@gmail.com'])
+   msg = Message('Hello', sender = config('MAIL_USERNAME'), recipients = ['rajulbhargav23@gmail.com','001adarshshrivastava@gmail.com','hashukumar01@gmail.com'])
    msg.body = "Hello Flask message sent from Flask-Mail"
    mail.send(msg)
    return "Sent"
